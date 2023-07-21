b3a3eaeeda6234761e6acb2bb770c32304944312
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-07-20 23:35:04 -0300

    advances

--- src/controllers/car.controllers.js
+++ src/controllers/car.controllers.js
@@ -6,6 +6,41 @@ const getAll = catchError(async (req, res) => {
   return res.json(car);
 });
 
+const create = catchError(async (req, res) => {
+  const car = await Car.create(req.body);
+  return res.status(201).json(car);
+});
+
+const getOne = catchError(async (req, res) => {
+  const { id } = req.params;
+  const car = await Car.findByPk(id);
+  if (!car) return res.sendStatus(400);
+  return res.json(car);
+});
+
+const destroy = catchError(async (req, res) => {
+  const { id } = req.params;
+  const carDestroy = await Car.destroy({ where: { id } });
+  if (!carDestroy) return res.sendStatus(400);
+  return res.sendStatus(204);
+});
+
+const update = catchError(async (req, res) => {
+  const { id } = req.params;
+
+  const car = await Car.update(req.body, {
+    where: { id },
+    returning: true,
+  });
+  if (car[0] === 0) return res.sendStatus(400);
+  return res.json(car[1][0]);
+});
+
 module.exports = {
   getAll,
+  // paso 4: recuerden de exportar
+  create,
+  getOne,
+  destroy,
+  update,
 };
--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,8 +1,21 @@
-const { getAll } = require("../controllers/car.controllers");
+const {
+  getAll,
+  create,
+  getOne,
+  destroy,
+  update,
+} = require("../controllers/car.controllers");
 const express = require("express");
 
 const carRouter = express.Router();
 
-carRouter.route("/").get(getAll);
+carRouter
+  .route("/")
+  .get(getAll)
+  // paso 5, creo el post
+  .post(create);
+
+//cars/id
+carRouter.route("/:id").get(getOne).delete(destroy).put(update);
 
 module.exports = carRouter;
